8389636434c23d10057380b12a0e51b0138da6f9
katy@karstaway.com <katy@karstaway.com>
2018-09-02 11:27:48 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,7 +2,7 @@ def count_elements(array)
  new_hash = {}
  count = 0
     array.each do |key|
-      if key.to_s == new_hash.keys
+      if new_hash.has_key? == new_hash.keys
         count += 1
       new_hash[key.to_s] = count
       new_hash
